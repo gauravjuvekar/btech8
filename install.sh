2b64e8b20efc7df9256b65d7d8bf5127fd6ddcad
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-05-10 16:44:20 +0530

    Link to datasets

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 
 DATA_DIR="$1"
 
+
+function setup_all() {
+    cd "$1"
+    pipenv sync --verbose
+    setup_s2v
+    cd ..
+}
+
+function link_data() {
+    ln -s "$1" data
+}
+
 function setup_s2v() {
     cd sent2vec/src
     pipenv run python setup.py build
@@ -14,12 +26,15 @@ git submodule update --init --recursive
 sudo apt-get install -y python3-pip cython3 python3-dev  python-pip cython python-dev build-essential
 pip3 install pipenv
 
+setup_all ./wsd
 cd wsd
-pipenv sync --verbose
-setup_s2v
+link_data "$DATA_DIR"
 cd ..
 
+
+setup_all ./extractive
 cd extractive
-pipenv sync --verbose
-setup_s2v
+link_data "$DATA_DIR"
 cd ..
+
+
