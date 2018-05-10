f3a4ffd0178863612073229e624faa5e7817ce29
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-05-10 16:06:12 +0530

    Update install

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+DATA_DIR="$1"
+
 git submodule update --init --recursive
+
+sudo apt-get install -y python3-pip cython3 python3-dev  python-pip cython python-dev
+pip3 install pipenv
+
+cd wsd
+pipenv sync
+
+cd ..
+
+cd extractive
+pipenv sync
+
+
