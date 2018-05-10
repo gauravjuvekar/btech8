a944fe6471bea0980e509de583756ca7ec1ab86a
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-05-10 16:28:13 +0530

    Compile sent2vec

--- install.sh
+++ install.sh
@@ -2,17 +2,24 @@
 
 DATA_DIR="$1"
 
+function setup_s2v() {
+    cd sent2vec/src
+    pipenv run python setup.py build
+    pipenv run python setup.py install
+    cd ../..
+}
+
 git submodule update --init --recursive
 
-sudo apt-get install -y python3-pip cython3 python3-dev  python-pip cython python-dev
+sudo apt-get install -y python3-pip cython3 python3-dev  python-pip cython python-dev build-essential
 pip3 install pipenv
 
 cd wsd
-pipenv sync
-
+pipenv sync --verbose
+setup_s2v
 cd ..
 
 cd extractive
-pipenv sync
-
-
+pipenv sync --verbose
+setup_s2v
+cd ..
